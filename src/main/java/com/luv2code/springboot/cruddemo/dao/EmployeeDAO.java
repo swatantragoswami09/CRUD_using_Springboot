1218c1a61d9b03f5aac5342d473f19c16ef57147
swatantra <swatantra@GS-2338>
2021-01-28 15:24:03 +0530

    first commit

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAO.java
@@ -5,7 +5,11 @@ import java.util.List;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 
 public interface EmployeeDAO {
+	
 	public List<Employee> findAll();
+	public Employee findById(int theId);
+	public void save(Employee theEmployee);
+//	public void deleteById(int theId);
 	
 
 }
